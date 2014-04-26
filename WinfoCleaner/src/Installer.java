5ebe24604e8cecaaf592b40be609359255cd6321
Philipp <Philipp@Mitgard>
2014-04-26 14:50:04 +0200

    Auswahl für welchen User die Skripte installiert werden sollen hinzugefügt

--- WinfoCleaner/src/Installer.java
+++ WinfoCleaner/src/Installer.java
@@ -1,24 +1,40 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Enumeration;
 
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.WindowConstants;
 
 
 public class Installer {
-	private final static String STARTUP_PATH = "C:\\Users\\Pohligpool\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
+	private static String user = "";
+	private static ButtonGroup group = new ButtonGroup();
+	private static JFrame frame = new JFrame("Select user");
+	private static String startupPath;
 	
 	public static void main(String[] args) {
 		String[] filenames = {"WinfoCleaner.jar", "RecycleBinCleaner.cmd"}; //Name der Files die Installiert werden sollen
+		setUser();
+		startupPath = "C:\\Users\\"+user+"\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
 		
 		String sourcePath = findInstallerPath(filenames);
 		
 		try {
 			for(String filename : filenames)
-				Files.copy(Paths.get(sourcePath, filename), Paths.get(STARTUP_PATH, filename));
+				Files.copy(Paths.get(sourcePath, filename), Paths.get(startupPath, filename));
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, e);;
+			JOptionPane.showMessageDialog(null, e);
 			e.printStackTrace();
 		}
 		
@@ -26,8 +42,46 @@ public class Installer {
 	}
 	
 	private static void printStartupContent(){
-		File startup = new File(STARTUP_PATH);
+		File startup = new File(startupPath);
 		JOptionPane.showMessageDialog(null, startup.list());
+		System.exit(0);
+	}
+	
+	private static void setUser(){
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		File usersDir= new File("C:\\Users");
+		JRadioButton button;
+		frame.getContentPane().setLayout(new GridLayout(usersDir.listFiles().length+2, 1));
+		
+		frame.getContentPane().add(new JLabel("Bitte User w\u00e4hlen f\u00fcr den die Programme installiert werden sollen"));
+		
+		for(File file : usersDir.listFiles()){
+			button = new JRadioButton(file.getName());
+			group.add(button);
+
+			frame.getContentPane().add(button);
+		}
+		
+		JButton button2 = new JButton("Ok");
+		
+		button2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				Enumeration<AbstractButton> enu = group.getElements();
+				
+				while(enu.hasMoreElements()){
+					AbstractButton button = enu.nextElement();
+					
+					if(button.isSelected())
+						user = button.getText();
+				}
+				frame.setVisible(false);
+			}
+		});
+
+		frame.getContentPane().add(button2);
+		frame.pack();
+		frame.setVisible(true);
+		while(frame.isVisible());
 	}
 	
 	/**
